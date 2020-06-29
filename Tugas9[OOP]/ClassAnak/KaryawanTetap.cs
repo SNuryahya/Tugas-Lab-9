81d692eed5d78e2fbae1815ce0b127f3d9aadf9c
SNuryahya <stya58@gmail.com>
2020-06-29 19:47:34 +0700

    Class Anak

---
+++ Tugas9[OOP]/ClassAnak/KaryawanTetap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using Tugas9.ClassInduk;
+
+namespace Tugas9.ClassAnak
+{
+    public class KaryawanTetap : Karyawan
+    {
+        public double Income { get; set; }
+        public override double Pendapatan()
+        {
+            return Income;
+        }
+    }
+}
---
+++ Tugas9[OOP]/ClassAnak/Sales.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using Tugas9.ClassInduk;
+
+namespace Tugas9.ClassAnak
+{
+    public class Sales : Karyawan
+    {
+        public double Sales_amount { get; set; }
+        public double Bonus { get; set; }
+        public override double Pendapatan()
+        {
+            return Sales_amount * Bonus;
+        }
+    }
+}
